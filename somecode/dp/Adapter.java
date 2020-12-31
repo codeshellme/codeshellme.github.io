a1d714885556f21cf707689e105b9f2c044fcf70
codeshellme <3101968978@qq.com>
2020-12-31 17:24:36 +0800

    x

---
+++ somecode/dp/Adapter.java
@@ -0,0 +1,48 @@
+public class Adapter {
+    public static void xiaoMing(BicycleInterface b) {
+        b.ride();
+    }
+
+    public static void main(String[] args) {
+        Bicycle b = new Bicycle();
+        Car c = new Car();
+
+        xiaoMing(b);
+        // xiaoMing(c); //类型不匹配
+
+        CarAdapter ca = new CarAdapter(c); // 将 Car 适配成 Bicycle
+        xiaoMing(ca); // 小明可以用了
+    }
+}
+
+class CarAdapter implements BicycleInterface {
+    private Car car;
+
+    public CarAdapter(Car car) {
+        this.car = car;
+    }
+
+    public void ride() {
+        car.drive();
+    }
+}
+
+interface BicycleInterface {
+    void ride();
+}
+
+class Bicycle implements BicycleInterface {
+    public void ride() {
+        System.out.println("I am riding a bicycle.");
+    }
+}
+
+interface CarInterface {
+    void drive();
+}
+
+class Car implements CarInterface {
+    public void drive() {
+        System.out.println("I am driving a car.");
+    }
+}
